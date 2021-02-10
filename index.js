a5ff270aff390a3ec0bb6bf9da9dd0109a215ccd
Connor Stec <stec.connor@gmail.com>
2021-02-10 13:28:17 -0500

    first commit
    - dummy flattenator with some initial tests to build toward

---
+++ index.js
@@ -0,0 +1,7 @@
+export default function flattenator({
+  inputObject,
+  nestingKey,
+  persistVals,
+}) {
+  return true;
+}
---
+++ testrunner.js
@@ -0,0 +1,37 @@
+import assert from 'assert';
+import flattenator from './index.js';
+
+const config = {};
+
+// It defaults output to an empty array
+assert.deepStrictEqual(flattenator(config), []);
+
+// It flattens a 2D array
+config.inputObject = [1, [2, 3]];
+assert.deepStrictEqual(flattenator(config), [1, 2, 3]);
+
+// It flattens a 3D array
+config.inputObject = [1, [2, [3, 4]]];
+assert.deepStrictEqual(flattenator(config), [1, 2, 3, 4]);
+
+// It flattens nested objects
+const parent = {id: 1, name: 'boop'};
+const child1 = {id: 2, name: 'snoop'};
+const child2 = {id: 3, name: 'zoop'};
+
+config.inputObject = {
+  ...parent,
+  children: [
+    ...child1,
+    ...child2,
+  ]
+};
+assert.deepStrictEqual(flattenator(config), [...parent, ...child1, ...child2]);
+
+// It returns only the desired properties per flattened object
+config.persistVals = ['name'];
+assert.deepStrictEqual(flattenator(config), [
+  {name: 'boop'},
+  {name: 'snoop'},
+  {name: 'zoop'},
+]);
